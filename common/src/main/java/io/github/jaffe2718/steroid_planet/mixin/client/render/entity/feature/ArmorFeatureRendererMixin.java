b1e5bd234bb30c1e947d1f5a687fa0706082f03f
Jaffe2718 <qqyttwqeei@163.com>
2026-02-25 14:00:30 +0800

    neoforge & fabric: mixin compatibility

--- common/src/main/java/io/github/jaffe2718/steroid_planet/mixin/client/render/entity/feature/ArmorFeatureRendererMixin.java
+++ common/src/main/java/io/github/jaffe2718/steroid_planet/mixin/client/render/entity/feature/ArmorFeatureRendererMixin.java
@@ -11,8 +11,9 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Equipment;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,27 +21,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ArmorFeatureRenderer.class)
 public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, A extends BipedEntityModel<T>> {
 
-    @Inject(method = "renderArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/feature/ArmorFeatureRenderer;setVisible(Lnet/minecraft/client/render/entity/model/BipedEntityModel;Lnet/minecraft/entity/EquipmentSlot;)V", shift = At.Shift.AFTER))
-    private void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci) {
-        if (model instanceof ArmorEntityModel<?>) {
-            BaseEntityModelAccessor armorModel = (BaseEntityModelAccessor) model;
-            this.steroid_planet$setVisible(entity, armorModel, armorSlot);
-            if (entity instanceof PlayerEntity
-                    && ((PlayerAttributeAccessor) entity).getMuscle() >= HealthConditionCriterion.MUSCLE_THRESHOLD) {
-                armorModel.strengthenArms();
-            } else {
-                armorModel.resetArms();
-            }
-        }
-    }
+    @Shadow
+    protected abstract A getModel(EquipmentSlot slot);
 
-    @Unique
-    private void steroid_planet$setVisible(T entity, BaseEntityModelAccessor model, EquipmentSlot armorSlot) {
-        switch (armorSlot) {
-            case HEAD -> model.getPointyHead().visible = entity instanceof PlayerEntity player
-                    && ((PlayerAttributeAccessor) player).getLiverHealth() < HealthConditionCriterion.LIVER_HEALTH_THRESHOLD;
-            case CHEST -> model.getPectoralMuscle().visible = entity instanceof PlayerEntity player
+    @SuppressWarnings("AmbiguousMixinReference")
+    @Inject(method = "render", at = @At("HEAD"))
+    private void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
+        if (this.getModel(EquipmentSlot.CHEST) instanceof ArmorEntityModel<?>
+                && livingEntity.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof Equipment equipment
+                && equipment.getSlotType() == EquipmentSlot.CHEST) {
+            BaseEntityModelAccessor chestplateModel = (BaseEntityModelAccessor) this.getModel(EquipmentSlot.CHEST);
+            chestplateModel.getPectoralMuscle().visible = livingEntity instanceof PlayerEntity player
                     && ((PlayerAttributeAccessor) player).getMuscle() >= HealthConditionCriterion.MUSCLE_THRESHOLD;
+        }
+        if (this.getModel(EquipmentSlot.HEAD) instanceof ArmorEntityModel<?>
+                && livingEntity.getEquippedStack(EquipmentSlot.HEAD).getItem() instanceof Equipment equipment
+                && equipment.getSlotType() == EquipmentSlot.HEAD) {
+            BaseEntityModelAccessor helmetModel = (BaseEntityModelAccessor) this.getModel(EquipmentSlot.HEAD);
+            helmetModel.getPointyHead().visible = livingEntity instanceof PlayerEntity player
+                    && ((PlayerAttributeAccessor) player).getLiverHealth() < HealthConditionCriterion.LIVER_HEALTH_THRESHOLD;
         }
     }
 
